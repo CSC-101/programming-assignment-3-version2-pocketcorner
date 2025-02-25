555a55c4e1823ab8c5808f960a5c26391566e752
Seven <ssuwandi@calpoly.edu>
2025-02-25 10:22:16 -0800

    First Commit

--- hw3.py
+++ hw3.py
@@ -1,43 +1,53 @@
+import build_data
 import data
 import county_demographics
 
 #Part 1
 #Comment
 def population_total(lst:list[data.CountyDemographics]) -> int:
+    pop_total = 0
+    for attrib in lst:
+        pop_total += attrib['Population']['2014 Population']
+    print(pop_total)
+    return pop_total
 
+population_total(county_demographics.get_report())
 
 #Part 2
 #Comment
-def filter_by_state(lst:list[data.CountyDemographics], abrv:str) -> list[data.CountyDemographics]:
-
-#Part 3
-#Comment
-def population_by_education(lst:list[data.CountyDemographics], edu_key:str) -> float:
-
-def population_by_ethnicity(lst:list[data.CountyDemographics], eth_key:str) -> float:
-
-def population_below_poverty_level(lst:list[data.CountyDemographics]) -> float:
-
-#Part 4
-#Comment
-def percent_by_education(lst:list[data.CountyDemographics], edu_key:str) -> float:
-
-def percent_by_ethnicity(lst:list[data.CountyDemographics], eth_key:str) -> float:
-
-def percent_below_poverty_level(lst: list[data.CountyDemographics]) -> float:
-
-#Part 5
-#Comment
-def education_greater_than(lst:list[data.CountyDemographics], edu_key:str, thresh:float) -> list[data.CountyDemographics]:
-
-def education_less_than(lst: list[data.CountyDemographics], edu_key: str, thresh:float) -> list[data.CountyDemographics]:
-
-
-def ethnicity_greater_than(lst: list[data.CountyDemographics], eth_key: str, thresh:float) -> list[data.CountyDemographics]:
-
-def ethnicity_less_than(lst: list[data.CountyDemographics], eth_key: str, thresh:float) -> list[data.CountyDemographics]:
-
-
-def below_poverty_level_greater_than(lst:list[data.CountyDemographics], thresh:float) -> list[data.CountyDemographics]:
-
-def below_poverty_level_less_than(lst: list[data.CountyDemographics], thresh: float) -> list[data.CountyDemographics]:
+# def filter_by_state(lst:list[data.CountyDemographics], abrv:str) -> list[data.CountyDemographics]:
+#
+#
+# #Part 3
+# #Comment
+# def population_by_education(lst:list[data.CountyDemographics], edu_key:str) -> float:
+#
+# def population_by_ethnicity(lst:list[data.CountyDemographics], eth_key:str) -> float:
+#
+# def population_below_poverty_level(lst:list[data.CountyDemographics]) -> float:
+#
+#
+# #Part 4
+# #Comment
+# def percent_by_education(lst:list[data.CountyDemographics], edu_key:str) -> float:
+#
+# def percent_by_ethnicity(lst:list[data.CountyDemographics], eth_key:str) -> float:
+#
+# def percent_below_poverty_level(lst: list[data.CountyDemographics]) -> float:
+#
+#
+# #Part 5
+# #Comment
+# def education_greater_than(lst:list[data.CountyDemographics], edu_key:str, thresh:float) -> list[data.CountyDemographics]:
+#
+# def education_less_than(lst: list[data.CountyDemographics], edu_key: str, thresh:float) -> list[data.CountyDemographics]:
+#
+#
+# def ethnicity_greater_than(lst: list[data.CountyDemographics], eth_key: str, thresh:float) -> list[data.CountyDemographics]:
+#
+# def ethnicity_less_than(lst: list[data.CountyDemographics], eth_key: str, thresh:float) -> list[data.CountyDemographics]:
+#
+#
+# def below_poverty_level_greater_than(lst:list[data.CountyDemographics], thresh:float) -> list[data.CountyDemographics]:
+#
+# def below_poverty_level_less_than(lst: list[data.CountyDemographics], thresh: float) -> list[data.CountyDemographics]:
